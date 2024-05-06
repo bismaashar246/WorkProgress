c6d0a6f030e3833ad9e0de71bb6421650c9caecc
Bisma Ashar <165661796+bismaashar246@users.noreply.github.com>
2024-05-06 22:14:42 +0500

    Update App.js

---
+++ Dynamic Forms/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import MyForm from "./MyForm";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Fill in information</h1>
+      <MyForm />
+    </div>
+  );
+}
